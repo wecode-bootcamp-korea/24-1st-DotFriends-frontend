fa1a8fba839bc74fbae72725afc0a415c25e87df
BobaeKeum <aann0221@gmail.com>
2021-08-30 17:38:24 +0900

    Add: 초기셋팅 업로드

---
+++ src/Routes.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
+import Main from './pages/Main/Main';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Main} />
+          <Route exact path="/signup" component={SignUp} />
+        </Switch>
+        <Footer />
+      </Router>
+    );
+  }
+}
+
+export default Routes;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <i class="far fa-moon"></i>
+        <h1>나는 무지랭이다</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
