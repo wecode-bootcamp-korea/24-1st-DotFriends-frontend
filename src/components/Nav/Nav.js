bcdb63a4ffee73487d9d6dc088177ccc75b21ede
jin <toto006521@gmail.com>
2021-09-02 09:23:16 +0900

    Add : nav 레이아웃 구현 중

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import './Nav.scss';
 
 class Nav extends Component {
   render() {
-    return <div></div>;
+    return (
+      <header className="nav">
+        <div className="naverMenu">
+          <div className="leftMenu">
+            <a href="https://www.naver.com/" alt="naver">
+              naver
+            </a>
+            <span>네이버 쇼핑</span>
+          </div>
+          <div className="rightMenu">
+            <button>찜한 스토어</button>
+            <button>마이페이지</button>
+            <button>장바구니</button>
+            <button>user</button>
+            <button>아이콘</button>
+          </div>
+        </div>
+        <div>
+          <h1>DOT FRIENDS</h1>
+        </div>
+      </header>
+    );
   }
 }
 
