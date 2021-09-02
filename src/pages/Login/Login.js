e9280147bd2cfc4076c5e0ca3cd90744ad2ba558
BobaeKeum <aann0221@gmail.com>
2021-09-02 17:45:55 +0900

    Add: 로그인 기능 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,12 +3,64 @@ import React, { Component } from 'react';
 import './Login.scss';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      pw: '',
+      btn: 'btnLogin',
+    };
+  }
+  onchangeId = e => {
+    this.setState({
+      id: e.target.value,
+    });
+  };
+  onchangePw = e => {
+    this.setState({
+      pw: e.target.value,
+    });
+  };
+  loginkey = () => {
+    if (this.state.id.includes('@') && this.state.pw.length > 7) {
+      this.setState({
+        btn: 'btnLoginActive',
+      });
+    } else {
+      this.setState({
+        btn: 'btnLogin',
+      });
+    }
+  };
+
+  handleLogin = () => {
+    fetch('http://10.58.4.133:8000/user/signin', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: this.state.id,
+        password: this.state.pw,
+      }),
+    })
+      .then(result => result.json())
+      .then(result => {
+        if (result.MESSAGE === 'SUCCESS') {
+          alert(`Welcome ${this.state.id}`);
+          console.log(result);
+          localStorage.setItem('dot-token', result.token);
+          this.props.history.push('/');
+        } else {
+          alert('Login failed');
+          console.log(result);
+        }
+      });
+  };
+
   render() {
     return (
       <div className="wrapLogin">
         <header className="header">
           <div className="headerInner">
-            <a href="#" className="logo">
+            <a href="/" className="logo">
               <h1 className="logoText">Dot Friends</h1>
             </a>
           </div>
@@ -18,25 +70,39 @@ class Login extends Component {
         <div className="content">
           <div className="loginWrap">
             <div className="idPwWrap">
-              <div className="idCell">
+              <div className="idPwCell">
                 <span className="iconSize">
-                  <i className="far fa-user"></i>
-                </span>
-                <input type="text" placeholder="아이디" className="inputId" />
-              </div>
-              <div className="pwCell">
-                <span className="iconSize">
-                  <i className="fas fa-unlock-alt"></i>
+                  <i className="far fa-user faIcon"></i>
                 </span>
                 <input
+                  className="inputIdPw"
+                  type="text"
+                  placeholder="아이디"
+                  value={this.state.id}
+                  onChange={this.onchangeId}
+                  loginkey={this.loginkey}
+                />
+              </div>
+              <div className="idPwCell">
+                <span className="iconSize">
+                  <i className="fas fa-unlock-alt faIcon"></i>
+                </span>
+                <input
+                  className="inputIdPw"
                   type="password"
                   placeholder="비밀번호"
-                  className="inputPw"
+                  value={this.state.pw}
+                  onChange={this.onchangePw}
+                  loginkey={this.loginkey}
                 />
               </div>
             </div>
             <div className="btnLoginWrap">
-              <button className="btnLogin btnLoginActive">로그인</button>
+              <a href="/">
+                <button className={this.state.btn} onClick={this.handleLogin}>
+                  로그인
+                </button>
+              </a>
             </div>
           </div>
           <ul className="findWrap">
