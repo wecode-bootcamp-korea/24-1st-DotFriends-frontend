cc1f8b60b344e119aa4fd2f11d56bd94e991fd28
jin <toto006521@gmail.com>
2021-09-03 13:40:27 +0900

    WIP : 네비바 레이아웃 구현중

---
+++ src/components/Nav/Category.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './Category.scss';
+
+class Category extends Component {
+  handleMenu = () => {};
+  render() {
+    const { category } = this.props;
+    const { subCategory } = category;
+    return (
+      <>
+        <li className="category">
+          <a href="#" key={category.id} className="categoryLink">
+            {category.title}
+            {subCategory && subCategory.length > 0 && (
+              <i className="fas fa-chevron-down" />
+            )}
+          </a>
+          <ul className="subCategories">
+            {category.subCategory &&
+              category.subCategory.map((sub, idx) => (
+                <li className="subCategory">
+                  <a href="#" key={idx}>
+                    {sub}
+                  </a>
+                </li>
+              ))}
+          </ul>
+        </li>
+      </>
+    );
+  }
+}
+
+export default Category;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,35 +1,141 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Category from './Category';
 import './Nav.scss';
 
 class Nav extends Component {
   render() {
     return (
       <header className="nav">
-        <div className="naverMenu">
-          <div className="leftMenu">
-            <a href="https://www.naver.com/" alt="naver">
-              naver
-            </a>
-            <span>네이버 쇼핑</span>
-          </div>
-          <div className="rightMenu">
-            <button>찜한 스토어</button>
-            <button>마이페이지</button>
-            <button>장바구니</button>
-            <button>user</button>
-            <button>아이콘</button>
+        <div className="topWrapper">
+          <div className="naverMenu">
+            <div className="leftMenu">
+              <a
+                href="https://www.naver.com/"
+                alt="naver"
+                className="naverLogo"
+              >
+                naver
+              </a>
+              <span className="shopping">
+                <i className="fas fa-shopping-bag" />
+                네이버 쇼핑
+              </span>
+            </div>
+            <div className="rightMenu">
+              <button className="login">로그인</button>
+              <button className="service">
+                <i className="fas fa-th" />
+              </button>
+            </div>
           </div>
         </div>
-        <div>
-          <h1>DOT FRIENDS</h1>
-          <button></button>
-          <form>
-            <input type="text" placeholder="검색어를 입력해보세요" />
+
+        <div className="headerMenu">
+          <button className="likedBtn">
+            <i className="fas fa-plus" />
+            찜하기 181,685
+          </button>
+          <h1 className="mainLogo">
+            <Link to="/">DOT FRIENDS</Link>
+          </h1>
+          <form className="searchForm">
+            <input
+              type="text"
+              className="search"
+              placeholder="검색어를 입력해보세요"
+            />
+            <button className="searchBtn">
+              <i className="fas fa-search" />
+            </button>
           </form>
         </div>
+        <nav className="categoriesContainer">
+          <div className="categoriesWrapper">
+            <ul className="categories">
+              {MENULIST.map(category => (
+                <Category category={category} />
+              ))}
+            </ul>
+            <button>더보기</button>
+          </div>
+        </nav>
       </header>
     );
   }
 }
 
 export default Nav;
+
+const MENULIST = [
+  {
+    id: 1,
+    title: '캐릭터',
+    subCategory: ['브라운앤프렌즈', 'BT21', '브롤스타즈'],
+  },
+  {
+    id: 2,
+    title: 'NEW',
+  },
+  {
+    id: 3,
+    title: 'SALE',
+  },
+  {
+    id: 4,
+    title: 'COLLABS',
+    subCategory: ['MINIONS', 'Masion Kisune', 'CASETiFY'],
+  },
+  {
+    id: 5,
+    title: '토이',
+    subCategory: ['중대형인형', '미니인형', '키링', '쿠션,방석', '피규어'],
+  },
+  {
+    id: 6,
+    title: '문구',
+    subCategory: [
+      '다이어리,플래너',
+      '노트,메모지',
+      '필기구,필통',
+      '스티커',
+      '마그넷',
+    ],
+  },
+  {
+    id: 7,
+    title: '디지털',
+    subCategory: ['폰케이스', '디지털케이스', '소형가전', '주변기기'],
+  },
+  {
+    id: 8,
+    title: '의류',
+    subCategory: ['반팔의류', '긴팔의류', '파자마', '양말,슬리퍼'],
+  },
+  {
+    id: 9,
+    title: '패션잡화',
+    subCategory: ['가방', '지갑,파우치', '뷰티', '패션소품', '위생용품'],
+  },
+  {
+    id: 10,
+    title: '리빙&레저',
+    subCategory: [
+      '패브릭,홈데코',
+      '컵,텀블러',
+      '주방,욕실',
+      '캠핑',
+      '골프',
+      '식품',
+    ],
+  },
+  {
+    id: 11,
+    title: '키즈',
+    subCategory: ['의류', '패션잡화', '양말,슬리퍼'],
+  },
+  {
+    id: 12,
+    title: '전체상품',
+  },
+];
