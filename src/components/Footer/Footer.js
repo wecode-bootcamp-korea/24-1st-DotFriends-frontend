415b283de51fb21835d77c933fe6c37c3270b08b
eunjeong-97 <beanlove97@gmail.com>
2021-09-04 10:26:53 +0900

    Add : Footer 레이아웃 구현중

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import './Footer.scss';
 
 class Footer extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="footer">
+        <div className="frontEnd">
+          <strong></strong>
+          <p></p>
+        </div>
+
+        <div className="backEnd">
+          <strong></strong>
+          <p></p>
+        </div>
+
+        <div className="wecode">
+          <strong></strong>
+          <p></p>
+        </div>
+      </div>
+    );
   }
 }
 
