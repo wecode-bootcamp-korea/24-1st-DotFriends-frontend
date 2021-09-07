9d7a192529ce4648fe01a2571bd3741ed671d522
eunjeong-97 <beanlove97@gmail.com>
2021-09-07 13:43:00 +0900

    Add: Footer 레이아웃 구현완료

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,10 @@ class Footer extends Component {
     return (
       <div className="footer">
         <div className="footerTitle">
-          <p>이 사이트는 Dot Friends의 1차 클론코딩 프로젝트 입니다!</p>
+          <p>
+            <i class="fas fa-exclamation-circle" /> 이 사이트는 Dot Friends의
+            1차 클론코딩 프로젝트 입니다!
+          </p>
           <p>제작자에 대해 궁금하면 하단의 깃허브 사이트를 참고해주세요</p>
         </div>
         <div className="footerContainer">
@@ -38,29 +41,48 @@ class Footer extends Component {
             <strong>backEnd 개발자</strong>
             <div className="text">
               <p>
-                <span className="name">Sally</span>
+                <span className="name">Moon</span>
                 <span> | </span>
                 <i class="fab fa-github" />
-                <span> https://github.com/BobaeKeum</span>
+                <span> https://github.com/shinwooju</span>
               </p>
               <p>
-                <span className="name">Leonard</span>
+                <span className="name">Brown</span>
                 <span> | </span>
                 <i class="fab fa-github" />
-                <span> https://github.com/JUCHEOLJIN</span>
+                <span> https://github.com/JackAwesomeKim</span>
               </p>
               <p>
-                <span className="name">Edward</span>
+                <span className="name">Boss</span>
                 <span> | </span>
                 <i class="fab fa-github" />
-                <span> https://github.com/eunjeong-97</span>
+                <span> https://github.com/sdk1926</span>
               </p>
             </div>
           </div>
 
           <div className="footerEle">
             <strong>wecode</strong>
-            <div className="text"></div>
+            <div className="text">
+              <p>
+                <span className="name">FrontEnd</span>
+                <span> | </span>
+                <i class="fab fa-github" />
+                <span>
+                  {' '}
+                  https://github.com/wecode-bootcamp-korea/24-1st-DotFriends-frontend
+                </span>
+              </p>
+              <p>
+                <span className="name">BackEnd</span>
+                <span> | </span>
+                <i class="fab fa-github" />
+                <span>
+                  {' '}
+                  https://github.com/wecode-bootcamp-korea/24-1st-DotFriends-backend
+                </span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
