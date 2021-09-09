e3cec16a35a364d32bd797e3938a2be807b63aa3
jin <toto006521@gmail.com>
2021-09-09 15:46:54 +0900

    Add : 간편보기 모달창 구현

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 // const BASE_URL = 'http://10.58.0.135:8000';
 export const BASE_URL = 'http://10.58.2.237:8000';
+// export const BASE_URL = 'http://10.58.4.139:8000/';
 export const PRODUCT_LIST_API = `${BASE_URL}/product?`;
 export const PRODUCT_API = `${BASE_URL}/product`;
